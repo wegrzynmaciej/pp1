35b60b5af4b3563f5a536635a3c6577eef769de5
Maciej Węgrzyn <wegrzyn.maciej@o2.pl>
2019-10-21 21:00:11 +0200

    update

--- 01-TypesAndVariables/25.py
+++ 01-TypesAndVariables/25.py
@@ -1,1 +1,18 @@
 number = input("Podaj nr rachunku bankowego: ")
+output = ''
+temp = ''
+try:
+    int(number)
+except ValueError:
+    print("Podaj tylko cyfry!")
+else:
+    numbers = [int(x) for x in str(number)]
+    print(numbers)
+    numbers.reverse()
+    print(numbers)
+    for x in numbers:
+        temp += str(x)
+        if len(temp)%4 == 0 or (len(temp)%4 == 2 and len(output) == 30):
+            output += (str(temp) + ' ')
+            temp = ''           
+    print(output[::-1])
