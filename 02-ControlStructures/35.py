c121bb7dcccb5917c3b265d266c5e1158154bc84
Maciej Węgrzyn <wegrzynmaciej@o2.pl>
2019-10-22 11:06:26 +0200

    Zadanie 02-35

--- 02-ControlStructures/35.py
+++ 02-ControlStructures/35.py
@@ -22,4 +22,20 @@ else:
         except (ValueError, NameError):
             print("Podana wartość nie jest prawidłową liczbą")
         else:
-            print("Równanie {0}x^2+{1}x+{2}=0 ".format(a,b,c))
+            result = "Równanie {0}x^2+{1}x+{2}=0 ".format(a,b,c)
+            a = float(a)
+            b = float(b)
+            c = float(c)
+            delta = b**2-4*a*c
+            if delta == 0:
+                x0 = (-b)/2*a
+                result += "ma jedno rozwiązanie {0}".format(x0)   
+                print(result)
+            elif delta < 0:
+                result += "nie ma rozwiązań"
+            else:
+                x1 = ((-b)-sqrt(delta))/2*a
+                x2 = ((-b)+sqrt(delta))/2*a
+                result += "ma 2 rozwiązania: {0} oraz {1}".format(x1,x2)
+            print(delta)
+            print(result)
