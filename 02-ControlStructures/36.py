6f955ea114586277db4efc6d639733f7b73457af
Maciej Węgrzyn <wegrzynmaciej@o2.pl>
2019-10-22 13:10:56 +0200

    02-38

---
+++ 02-ControlStructures/36.py
@@ -0,0 +1,12 @@
+# liczba podzielna przez 7, przy dzieleniu przez 2,3,4,5,6 daje reszte 1
+
+liczba = 0
+flag = False
+while flag != True:    
+    if liczba % 7 == 0 and liczba % 6 == 1 and liczba % 5 == 1 and liczba % 4 == 1 and liczba % 3 == 1 and liczba % 2 == 1:
+        flag = True
+    else:
+        flag = False
+        liczba += 1
+
+print("Liczba {0} jest podzielna przez 7 oraz przy dzieleniu przez 2,3,4,5,6 daje resztę 1.".format(liczba))
---
+++ 02-ControlStructures/38.py
@@ -0,0 +1,9 @@
+# klawiatura numpad
+i=6
+while i>=0:
+    j=1
+    while j<4:
+        print(f' {i+j}',end='')
+        j += 1
+    print()
+    i -= 3
