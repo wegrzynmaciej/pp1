dc9865b093d5529be9f98b565737f8973ff88023
Maciej Węgrzyn <wegrzynmaciej@o2.pl>
2019-10-22 10:53:43 +0200

    update

--- 02-ControlStructures/35.py
+++ 02-ControlStructures/35.py
@@ -1,2 +1,25 @@
+# coding=utf-8
 # pierwiastki równania ax^2+bx+c=0
-print('asd')
+from math import sqrt
+
+
+
+a = input("Podaj a: ")
+try:
+    float(a)
+except (ValueError, NameError):
+    print("Podana wartość nie jest prawidłową liczbą")
+else:
+    b = input("Podaj b: ")
+    try:
+        float(b)
+    except (ValueError, NameError):
+        print("Podana wartość nie jest prawidłową liczbą")
+    else:
+        c = input("Podaj c: ")
+        try:
+            float(c)
+        except (ValueError, NameError):
+            print("Podana wartość nie jest prawidłową liczbą")
+        else:
+            print("Równanie {0}x^2+{1}x+{2}=0 ".format(a,b,c))
