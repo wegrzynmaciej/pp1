ed22e278d38e37e882817d41927572b0c419b6e5
Maciej Węgrzyn <wegrzyn.maciej@o2.pl>
2019-10-24 20:34:21 +0200

    01 21 update

--- 01-TypesAndVariables/21.py
+++ 01-TypesAndVariables/21.py
@@ -1,7 +1,7 @@
 # Celsjusz na Kelvin
 
 try:
-    x = int(input('Podaj temperaturę w C: '))
-    print(f'{x}C to {float(x)+273.15}')
+    x = int(input("Podaj temperaturę w C: "))
+    print(f"{x}°C to {float(x)+273.15}°K lub {float(x)*1.8 + 32}°F")
 except ValueError:
-    print('Podana wartość nie jest liczbą!')
+    print("Podana wartość nie jest liczbą!")
