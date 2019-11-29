4c223487296117b1cb48fd5f6badb3e233519c7b
Maciej Węgrzyn <wegrzyn.maciej@o2.pl>
2019-11-29 13:03:31 +0100

    poprawa błędu

--- 03-FileHandling/19.py
+++ 03-FileHandling/19.py
@@ -1,10 +1,10 @@
 tab = []
-with open('03-FileHandling/universities.txt', 'r') as file:
+with open('03-FileHandling/universities_backup.txt', 'r') as file:
     for line in file:
-        tab.append(line.strip('"'))
+        tab.append(line.strip('"\n'))
 
 tab.sort()
-
+print(tab)
 with open('03-FileHandling/universities.txt', 'w') as file:
     for item in tab:
-        file.write(item)
+        file.write(item + '\n')
