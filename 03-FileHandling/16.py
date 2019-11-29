81fe77dd1c4c4f7f1bba51fc7cdd1ca1e9609999
Maciej Węgrzyn <wegrzyn.maciej@o2.pl>
2019-11-29 12:52:13 +0100

    poprawa błędu

--- 03-FileHandling/16.py
+++ 03-FileHandling/16.py
@@ -1,6 +1,7 @@
 import re
 
 komunikat = 'wtorek -23C, środa -21C, czwartek 25C'
-temp = re.findall('[-]*\d{2}C', komunikat)
-for t in temp:
-    print(t)
+temp = re.findall(r'[-]*\d{2}', komunikat)
+srednia = sum([int(x) for x in temp])/3
+
+print('{:.2f}'.format(srednia))
