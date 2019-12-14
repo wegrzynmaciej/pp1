f2237361fb8b653e6d6d2a6f933cfb8b2071dc09
Maciej Węgrzyn <wegrzyn.maciej@o2.pl>
2019-12-14 15:52:29 +0100

    07

--- 07-ObjectOrientedProgramming/point.py
+++ 07-ObjectOrientedProgramming/point.py
@@ -1,6 +1,16 @@
 class Point():
-    def __init__(self, x,y):
+    def __init__(self, x, y):
         self.x = x
         self.y = y
+
     def __str__(self):
         return f'P({self.x},{self.y})'
+
+    def __eq__(self, other):
+        return self.x == other.x and self.y == other.y
+
+
+p1 = Point(1, 2)
+p2 = Point(5, 6)
+p3 = Point(1, 2)
+print(p1 == p3)
