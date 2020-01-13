e6e14bf26420fc4f265085d952d31a92ca99455f
Maciej Węgrzyn <wegrzyn.maciej@o2.pl>
2020-01-13 09:33:12 +0100

    unittesting 6

--- 10-SoftwareTesting/classes.py
+++ 10-SoftwareTesting/classes.py
@@ -1,3 +1,6 @@
+from random import randint
+
+
 class Miasto():
 
     def __init__(self, nazwa, populacja):
@@ -31,3 +34,43 @@ class Zbiory():
     @staticmethod
     def roznica(a, b):
         return (a-b)
+
+
+class Macierz():
+
+    def __init__(self, m, n):
+        self.value = [[randint(0, 9) for _ in range(n)] for _ in range(m)]
+
+    def print(self):
+        for row in self.value:
+            print('[ ' + ''.join(["{:<3}"]*len(row)).format(*row) + ']')
+
+    def __add__(self, other):
+        if len(self.value) == len(other.value) and len(self.value[0]) == len(other.value[0]):
+            temp = Macierz(len(self.value), len(self.value[0]))
+            r = 0
+            for row in self.value:
+                e = 0
+                for _ in row:
+                    temp.value[r][e] = self.value[r][e] + other.value[r][e]
+                    e += 1
+                r += 1
+            return temp
+        else:
+            return Macierz(0, 0)
+
+
+if __name__ == "__main__":
+
+    m1 = Macierz(3, 5)
+    m2 = Macierz(3, 5)
+    m3 = Macierz(5, 3)
+    m4 = Macierz(6, 6)
+    m5 = Macierz(0, 0)
+    m7 = Macierz(3, 2)
+    m6 = m1 + m7
+    m1.print()
+    print()
+    m2.print()
+    print()
+    m6.print()
--- 10-SoftwareTesting/tests.py
+++ 10-SoftwareTesting/tests.py
@@ -44,17 +44,68 @@ class Test3(unittest.TestCase):
 
 
 class Test4(unittest.TestCase):
-    def test_zbiory_dzialania(self):
+    def test_zbiory_iloczyn(self):
         # Test klasy Zbiory
-        # czy działania działają poprawnie
+        # czy iloczyn działa poprawnie
         a = {1, 2, 3, 4}
         b = {2, 4, 6, 8}
         iloczyn = classes.Zbiory.iloczyn(a, b)
-        suma = classes.Zbiory.suma(a, b)
-        roznica = classes.Zbiory.roznica(a, b)
+
         self.assertEqual(iloczyn, {2, 4})
-        self.assertEqual(suma, {1, 2, 3, 4, 6, 8})
+
+    def test_zbiory_roznica(self):
+        # Test klasy Zbiory
+        # czy różnica działa poprawnie
+        a = {1, 2, 3, 4}
+        b = {2, 4, 6, 8}
+        roznica = classes.Zbiory.roznica(a, b)
         self.assertEqual(roznica, {1, 3})
+
+
+class Test6(unittest.TestCase):
+    def test_create_matrix_m_n(self):
+        # Test klasy Macierz
+        # Czy tworzona macierz jest o podanych wymiarach MxN
+        m = 7
+        n = 9
+        matrix = classes.Macierz(m, n)
+        self.assertEqual(len(matrix.value), m)
+        self.assertEqual(len(matrix.value[0]), n)
+
+    def test_add_matrixes(self):
+        # Test klasy Macierz
+        # Czy dodawanie macierzy zwraca poprawne wyniki
+        m = 7
+        n = 9
+        matrix1 = classes.Macierz(m, n)
+        matrix2 = classes.Macierz(m, n)
+        matrix_sum_success = matrix1 + matrix2
+        r = 0
+        for row in matrix1.value:
+            e = 0
+            for _ in row:
+                self.assertEqual(
+                    matrix1.value[r][e]+matrix2.value[r][e], matrix_sum_success.value[r][e])
+                e += 1
+            r += 1
+        # Macierze różnych wymiarów
+        m = 3
+        n = 5
+        matrix3 = classes.Macierz(m, n)
+        matrix_sum_failure_full = matrix1 + matrix3
+        self.assertEqual(matrix_sum_failure_full.value, [])
+        # M takie samo, N różne
+        m = 7
+        n = 5
+        matrix3 = classes.Macierz(m, n)
+        matrix_sum_failure_on_n = matrix1 + matrix3
+        self.assertEqual(matrix_sum_failure_on_n.value, [])
+        # M różne, N takie samo
+        m = 3
+        n = 9
+        matrix3 = classes.Macierz(m, n)
+        matrix_sum_failure_on_m = matrix1 + matrix3
+        self.assertEqual(matrix_sum_failure_on_m.value, [])
 
 
 if __name__ == "__main__":
